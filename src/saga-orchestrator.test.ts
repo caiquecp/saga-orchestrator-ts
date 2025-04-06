28f4ae2c5c47886e8e677c206b7795c35712322e
Caique Pereira <caique_cp@hotmail.com>
2025-04-06 14:36:00 +0100

    test: add one more unit test, compensate error use case

--- src/saga-orchestrator.test.ts
+++ src/saga-orchestrator.test.ts
@@ -43,6 +43,13 @@ const debit: TransactionDefinition<Ticket> = {
   },
 };
 
+const debitForceErrorCompensate: TransactionDefinition<Ticket> = {
+  ...debit,
+  compensate: (_ctx) => {
+    return Promise.reject(new Error("Unexpected error"));
+  },
+};
+
 const activate: TransactionDefinition<Ticket> = {
   name: "Activate",
   execute: (ctx) => {
@@ -56,13 +63,9 @@ const activate: TransactionDefinition<Ticket> = {
 };
 
 const activateForceError: TransactionDefinition<Ticket> = {
-  name: "Activate",
+  ...activate,
   execute: (_ctx) => {
     return Promise.reject(new Error("Unexpected error"));
-  },
-  compensate: (ctx) => {
-    ctx.status = "FAILED";
-    return Promise.resolve(ctx);
   },
 };
 
@@ -116,12 +119,43 @@ Deno.test("test SagaOrchestrator execute for a failure path - triggering compens
       restoreId: "bd972387-6df6-53bf-8fec-d484e4e6bdc9",
     },
   });
-  assertEquals(
-    res.compensateTransactionLogs,
-    [
-      { status: "compensated", transactionName: "Activate" },
-      { status: "compensated", transactionName: "Debit" },
-      { status: "compensated", transactionName: "CreateCode" },
-    ],
+  assertEquals(res.compensateTransactionLogs, [
+    { status: "compensated", transactionName: "Activate" },
+    { status: "compensated", transactionName: "Debit" },
+    { status: "compensated", transactionName: "CreateCode" },
+  ]);
+});
+
+Deno.test("test SagaOrchestrator compensate handling of failures - must keep going", async () => {
+  const initialData: Ticket = {
+    id: "cf9a761c-b06b-54d9-a8c2-d576c9506140",
+    status: "CREATED",
+    value: 5,
+    debit: {},
+  };
+  const saga = new SagaOrchestrator(
+    [createCode, debitForceErrorCompensate, activateForceError],
+    initialData,
   );
+  const res = await saga.execute();
+
+  assertEquals(res.success, false);
+  assertEquals(
+    res.error,
+    "Generic error, check compensate transaction logs for more details",
+  );
+  assertEquals(res.data, {
+    id: "cf9a761c-b06b-54d9-a8c2-d576c9506140",
+    code: "NO_CODE",
+    status: "FAILED",
+    value: 5,
+    debit: {
+      id: "dfefe41b-08d6-532a-a26c-a72b6a13473a",
+    },
+  });
+  assertEquals(res.compensateTransactionLogs, [
+    { status: "compensated", transactionName: "Activate" },
+    { status: "failed", transactionName: "Debit" },
+    { status: "compensated", transactionName: "CreateCode" },
+  ]);
 });
