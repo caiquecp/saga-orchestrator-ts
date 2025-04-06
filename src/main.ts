a051f30239391c719dd59d574f058fba6a4b8042
Caique Pereira <caique_cp@hotmail.com>
2025-04-06 10:51:00 +0100

    chore: format code, add some docs, etc

--- src/main.ts
+++ src/main.ts
@@ -1,81 +1,90 @@
 // deno-lint-ignore-file require-await
 
-import { SagaOrchestrator, TransactionDefinition } from "./another-saga-orchestrator.ts";
+import {
+  SagaOrchestrator,
+  TransactionDefinition,
+} from "./saga-orchestrator.ts";
 
 interface Ticket {
   id: string;
   code?: string | null;
   value: number;
-  status: 'CREATED' | 'ACTIVE' | 'USED' | 'FAILED';
+  status: "CREATED" | "ACTIVE" | "USED" | "FAILED";
   debit: {
     id?: string;
     reverseDebitId?: string;
-  }
+  };
 }
 
 const debitPoints: TransactionDefinition<Ticket> = {
-  name: 'DebitPoints',
+  name: "DebitPoints",
   execute: async (context) => {
-   context.debit.id = crypto.randomUUID();
-   return Promise.resolve(context);
+    context.debit.id = crypto.randomUUID();
+    return Promise.resolve(context);
   },
   compensate: async (context) => {
     context.debit.reverseDebitId = crypto.randomUUID();
     return Promise.resolve(context);
-  }
-}
+  },
+};
 
 const createCode: TransactionDefinition<Ticket> = {
-  name: 'CreateCode',
+  name: "CreateCode",
   execute: async (context) => {
-   context.code = crypto.randomUUID();
-   return Promise.resolve(context);
+    context.code = crypto.randomUUID();
+    return Promise.resolve(context);
   },
   compensate: async (context) => {
     context.code = null;
     return Promise.resolve(context);
-  }
-}
+  },
+};
 
 const activateTicket: TransactionDefinition<Ticket> = {
-  name: 'ActivateTicket',
+  name: "ActivateTicket",
   execute: async (context) => {
-   context.status = 'ACTIVE';
-   return Promise.resolve(context);
+    context.status = "ACTIVE";
+    return Promise.resolve(context);
   },
   compensate: async (context) => {
-    context.status = 'FAILED';
+    context.status = "FAILED";
     return Promise.resolve(context);
-  }
-}
+  },
+};
 
 const activateTicketForceFailure: TransactionDefinition<Ticket> = {
-  name: 'ActivateTicket',
-  execute: async (context) => {
-    throw new Error('Could not create ticket code');
-    context.status = 'ACTIVE';
-    return Promise.resolve(context);
+  name: "ActivateTicket",
+  execute: async (_context) => {
+    throw new Error("Could not create ticket code");
   },
   compensate: async (context) => {
-    context.status = 'FAILED';
+    context.status = "FAILED";
     return Promise.resolve(context);
-  }
-}
+  },
+};
 
 async function main() {
   const createTicketDto: Ticket = {
     id: crypto.randomUUID(),
     value: 5.0,
-    status: 'CREATED',
+    status: "CREATED",
     debit: {},
-  }
+  };
 
   try {
-    const sagaSuccess = new SagaOrchestrator([createCode, debitPoints, activateTicket], createTicketDto);
+    const sagaSuccess = new SagaOrchestrator([
+      createCode,
+      debitPoints,
+      activateTicket,
+    ], createTicketDto);
     const sagaSuccessRes = await sagaSuccess.execute();
     console.info(sagaSuccessRes);
 
-    const sagaFailure = new SagaOrchestrator([createCode, debitPoints, activateTicketForceFailure], createTicketDto);
+    const sagaFailure = new SagaOrchestrator([
+      createCode,
+      debitPoints,
+      activateTicketForceFailure,
+    ], createTicketDto);
     const sagaFailureRes = await sagaFailure.execute();
     console.info(sagaFailureRes);
   } catch (error) {
--- src/saga-orchestrator.ts
+++ src/saga-orchestrator.ts
@@ -1,69 +1,110 @@
-interface SagaContext<T> {
-  data: T;
-}
+// Requirements:
+// 1. it needs to go over an array of steps/transactions, execute each,
+// and if it fails rollback every step executed until the current one
+// 2. it needs to hold a context of data, which will be updated at each transaction
+// 3. it needs to be generic, at least related to the data type it'll work/manipulate
 
-interface SagaResult<T> {
-  success: boolean;
-  data?: T;
-  error?: string;
-  // compensations?: Array<{ step: string; status: 'completed' | 'failed' }>;
-}
+// Notes:
+// 1. at first, I'll make it accept, hold (as context) and resolve with the same data type
+// but ideally there should be a separation of what is the input, what is the context and what is the output
 
-interface SagaTransaction<T> {
+export interface TransactionDefinition<T> {
   name: string;
-  execute: (context: SagaContext<T>) => Promise<SagaContext<T>>;
-  compensate: (context: SagaContext<T>) => Promise<SagaContext<T>>;
+  execute: (context: T) => Promise<T>;
+  compensate: (context: T) => Promise<T>;
+}
+
+export type TransactionLogs = Array<
+  { transactionName: string; status: "completed" | "failed" | "compensated" }
+>;
+
+export interface SagaResult<T> {
+  success: boolean;
+  data: T;
+  error?: string;
+  compensateTransactionLogs?: TransactionLogs;
 }
 
 export class SagaOrchestrator<T> {
-  private steps: SagaTransaction<T>[];
-  private currentStepIndex: number;
-  private completedSteps: SagaTransaction<T>[];
-  private context: SagaContext<T>;
-  private isCompensating: boolean;
+  private transactions: TransactionDefinition<T>[];
+  private completedTransactions: TransactionDefinition<T>[];
+  private context: T;
+  private currentTransactionIndex: number;
+  private compensateTransactionLogs?: TransactionLogs;
 
-  constructor(steps: SagaTransaction<T>[], initialContextData: T) { 
-    this.steps = steps;
-    this.currentStepIndex = 0;
-    this.completedSteps = [];
-    this.context = { data: initialContextData };
-    this.isCompensating = false;
+  constructor(transactions: TransactionDefinition<T>[], initialData: T) {
+    this.transactions = transactions;
+    this.completedTransactions = [];
+    this.context = initialData;
+    this.currentTransactionIndex = 0;
   }
 
+  /**
+   * Execute the saga, going through every transaction - and handling compensate in case of failure.
+   */
   async execute(): Promise<SagaResult<T>> {
-    this.currentStepIndex = 0;
-    this.completedSteps = [];
-    this.isCompensating = false;
+    this.currentTransactionIndex = 0;
 
-    while (this.currentStepIndex < this.steps.length) {
-      const currentStep = this.steps[this.currentStepIndex];
+    while (this.currentTransactionIndex < this.transactions.length) {
+      const currentTransaction =
+        this.transactions[this.currentTransactionIndex];
 
       try {
-        this.context = await currentStep.execute(this.context);
-
-        this.completedSteps.push(currentStep)
-        this.currentStepIndex++;
+        this.context = await currentTransaction.execute(this.context);
+        this.completedTransactions.push(currentTransaction);
+        this.currentTransactionIndex++;
       } catch (error) {
-        console.error(`Saga step ${currentStep.name} failed:`, (error as Error).message);
+        console.error(error);
         return this.compensate();
       }
     }
 
-    return { success: true, data: this.context.data };
+    return {
+      success: true,
+      data: this.context,
+    };
   }
 
+  /**
+   * Coordinates the compensation for each transaction, from the one that failed and back.
+   * In case of a compensate call fails, it'll ignore it and keep comepensating past
+   * transaction - caller is responsible for handling any transaction that it was not
+   * possible to compensate.
+   */
   async compensate(): Promise<SagaResult<T>> {
-    this.isCompensating = true;
+    this.compensateTransactionLogs = [];
 
-    return { success: false, data: this.context.data, error: 'Oops...' }
-  }
-}
+    while (this.currentTransactionIndex >= 0) {
+      const currentTransaction =
+        this.transactions[this.currentTransactionIndex];
 
-export function sagaOrchestratorFactory<T>(partnership: string, input: T): SagaOrchestrator<T> {
-  switch (partnership) {
-    case 'BA_LESHUTTLE':
-      return new SagaOrchestrator<T>([], input);
-    default:
-      throw new Error('Unknown partnership, can\'t create the SagaOrchestrator');
+      try {
+        this.context = await currentTransaction.compensate(this.context);
+        this.compensateTransactionLogs.push({
+          transactionName: currentTransaction.name,
+          status: "compensated",
+        });
+        this.currentTransactionIndex--;
+      } catch (error) {
+        // what to do? terminate the orchestration and let the caller resolve
+        // (maybe tell what step failed, what have been compensated etc)?
+        // or should we try to compensate every step, even if some of them fail,
+        // and let the caller, the same as above, handle it?
+        console.error(error);
+        this.compensateTransactionLogs.push({
+          transactionName: currentTransaction.name,
+          status: "failed",
+        });
+        this.currentTransactionIndex--;
+        continue;
+      }
+    }
+
+    return {
+      success: false,
+      data: this.context,
+      error: "Generic error, check compensate transaction logs for more details",
+      compensateTransactionLogs: this.compensateTransactionLogs,
+    };
   }
 }
